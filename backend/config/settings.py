934afb4dd701d2a6507baaebc761f7ad9bfb9f6b
jacobwangari <mwasjac@gmail.com>
2026-08-25 09:24:50 +0300

    Changed pagination to a custom one

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -179,8 +179,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.CursorPagination',
-    'PAGE_SIZE': 20,
+    'DEFAULT_PAGINATION_CLASS': 'apps.common.pagination.CreatedAtCursorPagination',
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
